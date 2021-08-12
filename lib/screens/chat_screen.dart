88d281b71852c996dc3ed2378080f7f1507e97d6
Pushkar <pushkar.srivastava@infostride.com>
2021-08-13 02:05:34 +0530

    chat module,socket

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,5 @@
+import 'dart:convert';
 import 'dart:io';
-
 import 'package:app/common/colors.dart';
 import 'package:app/common/imagepickerhandler.dart';
 import 'package:app/common/sender_message.dart';
@@ -8,19 +8,15 @@ import 'package:app/common/textstyle.dart';
 import 'package:app/models/loader.dart';
 import 'package:app/providers/signin_provider.dart';
 import 'package:app/response/chat_details_response.dart';
-import 'package:app/screens/signature.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:app/common/get_it.dart';
 import 'package:app/common/navigator_route.dart';
 import 'package:app/common/navigator_service.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:provider/provider.dart';
-
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:socket_io_client/socket_io_client.dart';
-// import 'package:socket_io_client/socket_io_client.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -173,40 +169,6 @@ class _ChatScreenState extends State<ChatScreen>
   }
 
   // _connectSocket01() {
-  //   //update your domain before using
-  //   // Dart client
-  //   IO.Socket socket = IO.io(
-  //       'http://203.190.154.100:8087/',
-  //       OptionBuilder()
-  //           .setTransports(['websocket']) // for Flutter or Dart VM
-  //           .disableAutoConnect() // disable auto-connection
-  //           .setExtraHeaders({'foo': 'bar'}) // optional
-  //           .build());
-  //   socket.connect();
-  //
-  //   socket.onConnect((_) {
-  //     print('response----------------------------- connect');
-  //     // socket.emit('joinChannel', widget.tripId);
-  //   });
-  //
-  //   socket.on('getMessage', (data) {
-  //     //messageTextController.clear();
-  //
-  //     Data messageData = Data.fromJson(data);
-  //     print('msg----------------------------- ' + messageData.toString());
-  //     addMessagesToList(messageData);
-  //
-  //     // if (messages != null && messages.length > 2) {
-  //     //   itemScrollController.scrollTo(
-  //     //       index: messages.length - 1,
-  //     //       duration: Duration(milliseconds: 200),
-  //     //       curve: Curves.decelerate);
-  //     // }
-  //   });
-  //
-  //   socket.onDisconnect((_) => print('disconnect'));
-  //   socket.on('fromServer', (_) => print(_));
-  // }
 
   String profileImage =
       "https://cultivatedculture.com/wp-content/uploads/2019/12/LinkedIn-Profile-Picture-Example-Madeline-Mann.jpeg";
@@ -261,7 +223,7 @@ class _ChatScreenState extends State<ChatScreen>
         'http://3.142.72.8:3000/',
         OptionBuilder()
             .setTransports(['websocket']) // for Flutter or Dart VM
-            .disableAutoConnect() // disable auto-connection
+            .enableAutoConnect() // disable auto-connection
             .setExtraHeaders({'foo': 'bar'}) // optional
             .build());
     socket.connect();
@@ -277,56 +239,31 @@ class _ChatScreenState extends State<ChatScreen>
         "seen": false,
         "msgCategory": "collegue",
       };
-      // socket.emit("sendMessage", messageData);
       socket.emitWithAck("sendMessage", messageData, ack: (data) {
-        print('ack $data') ;
+        print('ack $data');
         if (data != null) {
           print('from server $data');
         } else {
-          print("Null") ;
+          print("Null");
         }
       });
-    // });
+    });
+    socket.on('data', (check) {
+      print(check.toString());
     });
     socket.on('getMessage', (data) {
       final dataList = data as List;
       final ack = dataList.last as Function;
-      print(dataList.toList().toString());
-      ack(null);
+      print(dataList.toString());
     });
     socket.on('getMessage', (data) {
-      //messageTextController.clear();
-      // print(data.toString());
+      print(jsonDecode(data.toString()));
       DataM res = DataM.fromJson(data);
       // print(res.toJson().toString());
       print('msg----------------------------- ${res.toString()}');
       addMessagesToList(res);
-
-      // if (messages != null && messages.length > 2) {
-      //   itemScrollController.scrollTo(
-      //       index: messages.length - 1,
-      //       duration: Duration(milliseconds: 200),
-      //       curve: Curves.decelerate);
-      // }
     });
-    // socket.on('getMessage', (data) {
-    //   //messageTextController.clear();
-    //
-    //   Data messageData = Data.fromJson(data);
-    //   print('msg----------------------------- ' + messageData.toString());
-    //   addMessagesToList(messageData);
-    //
-    //   // if(messages!=null && messages.length>2){
-    //   //   itemScrollController.scrollTo(
-    //   //       index: messages.length-1,
-    //   //       duration: Duration(milliseconds: 200),
-    //   //       curve: Curves.decelerate);
-    //   // }
-    // });
 
-    // socket.onDisconnect((_) => print('disconnect'));
-    // socket.on('getMessage', (_) => print(_));
-    // print(socket.json.toString());
     socket.on('event', (data) => print(data));
     socket.onDisconnect((_) => print('disconnect'));
     socket.on('fromServer', (_) => print(_));
@@ -339,7 +276,6 @@ class _ChatScreenState extends State<ChatScreen>
           // socket = io('http://127.0.0.1:3000',
           <String, dynamic>{
             'transports': ['websocket'],
-
             'autoConnect': false,
           });
 
@@ -348,8 +284,6 @@ class _ChatScreenState extends State<ChatScreen>
 
       // Handle socket events
       socket.on('connect', (_) => print('connect: ${socket.id}'));
-      // socket.on('location', handleLocationListen);
-      // socket.on('typing', handleTyping);
       socket.on('getMessage', (_) => print('message'));
       socket.on('disconnect', (_) => print('disconnect'));
       socket.on('fromServer', (_) => print(_));
@@ -358,51 +292,11 @@ class _ChatScreenState extends State<ChatScreen>
     }
   }
 
-// Send Location to Server
-// sendLocation(Map<String, dynamic> data) {
-//   socket.emit("location", data);
-// }
-
-// Listen to Location updates of connected usersfrom server
-// handleLocationListen(Map<String, dynamic> data) async {
-//   print(data);
-// }
-
-// Send update of user's typing status
-// sendTyping(bool typing) {
-//   socket.emit("typing",
-//       {
-//         "id": socket.id,
-//         "typing": typing,
-//       });
-// }
-
-// Listen to update of typing status from connected users
-// void handleTyping(Map<String, dynamic> data) {
-//   print(data);
-// }
-
-// Send a Message to the server
-// sendMessage(String message) {
-//   socket.emit("message",
-//     {
-//       "id": socket.id,
-//       "message": message, // Message to be sent
-//       "timestamp": DateTime.now().millisecondsSinceEpoch,
-//     },
-//   );
-// }
-
-// Listen to all message events from connected users
   void handleMessage(Map<String, dynamic> data) {
     print(data);
   }
 
   _connectSocket02() {
-    //update your domain before using
-    // Dart client
-    // IO.Socket socket = IO.io('http://3.142.72.8:3000/');
-
     IO.Socket socket = IO.io(
         'http://3.142.72.8:3000/',
         OptionBuilder()
@@ -411,72 +305,20 @@ class _ChatScreenState extends State<ChatScreen>
             .setExtraHeaders({'foo': 'bar'}) // optional
             .build());
 
-    // socket = io(
-    //   //'http://api.sunny-75.com:8087/',
-    //   //'http://10.10.30.20:8087/',
-    //   //   'http://sunny75.mobilytedev.com:8087/',
-    //     'http://3.142.72.8:3000/',
-    //     OptionBuilder()
-    //         .setTransports(['websocket']) // for Flutter or Dart VM
-    //         .disableAutoConnect() // disable auto-connection
-    //         .setExtraHeaders({'foo': 'bar'}) // optional
-    //         .build());
     socket.connect();
     socket.onConnect((_) {
       print('connect');
       socket.emit('joinChannel', 't45ts54pv045');
     });
 
-    // socket.onConnect((_) {
-    //   print('response----------------------------- connect');
-    //   socket.emit('joinChannel', 't45ts54pv045');
-    //   var messageData = {
-    //     "senderId": '60ef5bf342bfc70add091d4d',
-    //     "receiverId":'6101bfdb9836893913804b53',
-    //     "roomId": 't45ts54ui945',
-    //     "msg": 'hello sir',
-    //     "msgType": "text",
-    //     "seen":false,
-    //     "msgCategory" : "collegue",
-    //   };
-    //   socket.emit("sendMessage",
-    //       messageData);
-    //
-    // });
     socket.on('connect', (data) => print('Connectde...'));
     socket.on('getMessage', (data) {
-      //messageTextController.clear();
-      // print(data.toString());
       DataM res = DataM.fromJson(data);
       // print(res.toJson().toString());
       print('msg----------------------------- ${res.toString()}');
       addMessagesToList(res);
-
-      // if (messages != null && messages.length > 2) {
-      //   itemScrollController.scrollTo(
-      //       index: messages.length - 1,
-      //       duration: Duration(milliseconds: 200),
-      //       curve: Curves.decelerate);
-      // }
     });
-    // socket.on('getMessage', (data) {
-    //   //messageTextController.clear();
-    //
-    //   Data messageData = Data.fromJson(data);
-    //   print('msg----------------------------- ' + messageData.toString());
-    //   addMessagesToList(messageData);
-    //
-    //   // if(messages!=null && messages.length>2){
-    //   //   itemScrollController.scrollTo(
-    //   //       index: messages.length-1,
-    //   //       duration: Duration(milliseconds: 200),
-    //   //       curve: Curves.decelerate);
-    //   // }
-    // });
 
-    // socket.onDisconnect((_) => print('disconnect'));
-    // socket.on('getMessage', (_) => print(_));
-    // print(socket.json.toString());
     socket.on('event', (data) => print(data));
     socket.onDisconnect((_) => print('disconnect'));
     socket.on('fromServer', (_) => print(_));
